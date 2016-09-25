b8d5815a5d4c0d5a4f31b891294d86c4ac58be3e
Valon Hajredini <hajredini.valon@gmail.com>
2016-09-25 23:24:00 +0200

    Added dinamic roouts and variables

--- Core/Router.php
+++ Core/Router.php
@@ -8,17 +8,22 @@
  */
 class Router
 {
-    protected  $routes  = [];
+    public  $routes  = [];
 //    Parameters from the match routes
-    protected  $params  = [];
+    public  $params  = [];
     /*
      * Add route to routing table
      * $param array $patam parameters (controller, action, rtc..)
      *
      * @routrn void
      * */
-    public function add($route, $params){
+    public function add($route, $params = []){
+        $route = preg_replace('/\//','\\/', $route);
+        $route = preg_replace('/\{([a-z]+)\}/','(?P<\1>[a-z-]+)', $route);
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+        $route = '/^'.$route.'$/i';
         $this->routes[$route] = $params;
+
     }
     public function getRoutes(){
         return $this->routes;
@@ -26,12 +31,26 @@ class Router
 
 //    Match the routes to root in the routing table
     public function match ($url){
-        foreach($this->routes as $route => $params):
-            if($url == $route):
+//        foreach($this->routes as $route => $params):
+//            if($url == $route):
+//                $this->params = $params;
+//                return true;
+//            endif;
+//        endforeach;
+//        $reg_ex = '/^(?P<Controller>[a-z-]+)\/(?P<Action>[a-z-]+$)/';
+        foreach ($this->routes as $route => $params){
+            if(preg_match($route, $url, $matches)) {
+                //        Het named capture
+//                $params = [];
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
                 $this->params = $params;
                 return true;
-            endif;
-        endforeach;
+            }
+        }
         return false;
     }
     public function getParams(){
--- public/index.php
+++ public/index.php
@@ -8,19 +8,43 @@ $router = new Router();
 //echo get_class($router);
 
 // Add the routes :
-
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
-
-//echo '<pre>';
-//    var_dump($router->getRoutes());
-//echo '</pre>';
 $url = $_SERVER['QUERY_STRING'];
-if($router->match($url)):
-    echo '<pre>';
-        var_dump($router->getParams());
-    echo '</pre>';
-else:
-    echo 'No route found for '.$url;
-endif;
+
+
+$router->add('posts/{id:\d+}/show', ['controller' => 'Posts', 'action' => 'show']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+
+
+echo '<pre>';
+//    var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
+
+//echo $url.'<br>';
+
+echo "-----------------------------------------<br>";
+
+//    echo $url.'<br>';
+    if($router->match($url)):
+        echo '<pre>';
+        print_r($router->getParams());
+        echo '</pre>';
+
+    else:
+        echo "Not a valide root <br>";
+    endif;
+echo "-----------------------------------------<br>";
+//foreach ($router->routes as $key=> $value):
+//    echo '<pre>';
+//echo 'Key=> '.$key.', Params: ';
+//    print_r($value);
+//    echo '</pre>';
+//
+//endforeach;
+//print_r($router->params);
+echo "-----------------------------------------";
